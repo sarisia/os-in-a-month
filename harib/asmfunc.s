8cf19f0977294969608134af6fc4acf072d2687a
Sarisia <sarisiaice@gmail.com>
2020-05-11 04:56:58 +0900

    harib01f

--- harib/asmfunc.s
+++ harib/asmfunc.s
@@ -1,13 +1,39 @@
+; .arch i486
 .code32
 
 # .text # リンカで .text はバイナリに突っ込まれる
 .global _io_hlt # _io_hlt を露出させないとリンカが bootpack.c での呼び出しとくっつけられない
-_io_hlt:
+_io_hlt: # void _io_hlt()
     hlt
     ret
 
+.global _io_load_eflags
+_io_load_eflags: # int _io_load_eflags()
+    pushf # Push eFLAGS Register onto the Stack
+    pop %eax
+    ret
+
+.global _io_store_eflags
+_io_store_eflags: # void _io_store_eflags(int eflags)
+    mov 4(%esp), %eax
+    push %eax
+    popf # Pop Stack into eFLAGS Register
+    ret
+
+.global _io_cli
+_io_cli: # void _io_cli()
+    cli # Clear Interrupt Flag
+    ret
+
+.global _io_out8
+_io_out8: # void _io_out8(int port, int data)
+    mov 4(%esp), %edx
+    mov 8(%esp), %al
+    out %al, %dx # OUT DX, AL: Output to Port
+    ret
+
 ; .global _write_mem8
-; _write_mem8:
+; _write_mem8: # void _write_mem8(int addr, int data)
 ;     mov 4(%esp), %ecx # addr
 ;     mov 8(%esp), %al # data
 ;     mov %al, (%ecx)
--- harib/bootpack.c
+++ harib/bootpack.c
@@ -1,14 +1,60 @@
 void _io_hlt(void);
+int _io_load_eflags();
+void _io_store_eflags(int eflags);
+void _io_cli();
+void _io_out8(int port, int data);
 // void _write_mem8(int addr, int data);
 
+void init_palette();
+void set_palette(int start, int len, unsigned char *table);
+
 void HariMain(void) {
+    init_palette();
+
     char *p = (char *)0xa0000;
 
     for (int i = 0; i <= 0xffff; i++) {
         p[i] = i & 0xf;
     }
 
-    for (;;) {
+    while (1) {
         _io_hlt();
     }
+}
+
+void init_palette() {
+    static unsigned char table_rgb[3*16] = {
+        0x00, 0x00, 0x00, /*  0:黒 */
+		0xff, 0x00, 0x00, /*  1:明るい赤 */
+		0x00, 0xff, 0x00, /*  2:明るい緑 */
+		0xff, 0xff, 0x00, /*  3:明るい黄色 */
+		0x00, 0x00, 0xff, /*  4:明るい青 */
+		0xff, 0x00, 0xff, /*  5:明るい紫 */
+		0x00, 0xff, 0xff, /*  6:明るい水色 */
+		0xff, 0xff, 0xff, /*  7:白 */
+		0xc6, 0xc6, 0xc6, /*  8:明るい灰色 */
+		0x84, 0x00, 0x00, /*  9:暗い赤 */
+		0x00, 0x84, 0x00, /* 10:暗い緑 */
+		0x84, 0x84, 0x00, /* 11:暗い黄色 */
+		0x00, 0x00, 0x84, /* 12:暗い青 */
+		0x84, 0x00, 0x84, /* 13:暗い紫 */
+		0x00, 0x84, 0x84, /* 14:暗い水色 */
+		0x84, 0x84, 0x84, /* 15:暗い灰色 */
+    };
+    set_palette(0, 15, table_rgb);
+}
+
+void set_palette(int start, int len, unsigned char *table) {
+    int eflags = _io_load_eflags();
+    _io_cli(); // clear interrupt flag
+    _io_out8(0x03c8, start);
+
+    for (int i = start; i < len; ++i) {
+        _io_out8(0x03c8, table[0] / 4);
+        _io_out8(0x03c8, table[1] / 4);
+        _io_out8(0x03c8, table[2] / 4);
+        table += 3;
+    }
+
+    _io_store_eflags(eflags);
 }

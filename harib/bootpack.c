3422e42444cbc030df5a1777e106caf5b90d8bed
Sarisia <sarisiaice@gmail.com>
2020-05-10 13:32:38 +0900

    harib01d

--- harib/bootpack.c
+++ harib/bootpack.c
@@ -2,10 +2,10 @@ void _io_hlt(void);
 // void _write_mem8(int addr, int data);
 
 void HariMain(void) {
-    char *p;
-    for (int i = 0xa0000; i <= 0xaffff; i++) {
-        p = (char *)i;
-        *p = i & 0xf;
+    char *p = (char *)0xa0000;
+
+    for (int i = 0; i <= 0xffff; i++) {
+        *(p + i) = i & 0xf;
     }
 
     for (;;) {
